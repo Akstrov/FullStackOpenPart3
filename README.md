da5ea932c48a0d0eb26f931a0b7b2c0e88dbe916
akstrov <yalam385.ya@gmail.com>
2023-06-01 16:04:36 +0100

    Add readMe

---
+++ README.md
@@ -0,0 +1,1 @@
+### Link to the application: https://phonebook-p79m.onrender.com
--- phonebook/index.js
+++ phonebook/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
